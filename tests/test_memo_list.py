fcab18d2f9e72e76dc4c20c244a4c997886a949d
prakashdubey-solytics <prakash.dubey@solytics-partners.com>
2026-04-30 16:15:12 +0530

    Revert "Automated PR from agent"

--- tests/test_memo_list.py
+++
@@ -1,7 +0,0 @@
-from playwright.sync_api import Page
-from utils import create_memo, unique_text
-
-class TestMemoList:
-    def test_memos_sorted_by_last_edited_desc(self, page: Page, base_url: str):
-        # create multiple memos, edit one, assert order
-        pass
